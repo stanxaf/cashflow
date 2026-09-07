d088d8483af543c2647a8d8ae198f77f8668fe92
SAF <stanleyfamilleza@gmail.com>
2026-09-07 20:57:15 +0700

    Add shared UI and formatting helpers

---
+++ src/lib/utils.ts
@@ -0,0 +1,23 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatPHP(amountInCentavos: number) {
+  return new Intl.NumberFormat("en-PH", {
+    style: "currency",
+    currency: "PHP",
+    maximumFractionDigits: 0,
+  }).format(amountInCentavos / 100);
+}
+
+export function formatDate(date: string, options?: Intl.DateTimeFormatOptions) {
+  return new Intl.DateTimeFormat("en-PH", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+    ...options,
+  }).format(new Date(`${date}T00:00:00+08:00`));
+}
