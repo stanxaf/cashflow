a85cce881992fa6c8f3a5b65b001d4ac5b72331c
SAF <stanleyfamilleza@gmail.com>
2026-09-08 10:41:52 +0700

    fix: redirect legacy new item route

--- src/app/(app)/items/new/page.tsx
+++ src/app/(app)/items/new/page.tsx
@@ -1,11 +1,5 @@
-import { FinancialItemForm } from "@/components/financial-item-form";
+import { redirect } from "next/navigation";
 
 export default function NewItemPage() {
-  return (
-    <div className="fixed inset-0 z-50 bg-black/20 sm:bg-black/25">
-      <div className="absolute inset-x-0 bottom-0 h-[88dvh] overflow-hidden rounded-t-2xl bg-background shadow-2xl sm:inset-y-0 sm:left-auto sm:right-0 sm:h-full sm:w-[460px] sm:rounded-none sm:border-l">
-        <FinancialItemForm />
-      </div>
-    </div>
-  );
+  redirect("/paydays?item=new");
 }
