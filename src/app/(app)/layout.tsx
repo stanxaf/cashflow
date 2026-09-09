cabb33fd8237e50cebfde8bba0936d8f62263a1f
SAF <stanleyfamilleza@gmail.com>
2026-09-09 11:09:36 +0700

    Provide shared prototype state

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,10 @@
 import { AppShell } from "@/components/app-shell";
+import { PrototypeStoreProvider } from "@/components/prototype-store";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return <AppShell>{children}</AppShell>;
+  return (
+    <PrototypeStoreProvider>
+      <AppShell>{children}</AppShell>
+    </PrototypeStoreProvider>
+  );
 }
