86f50bba3bcd6c31272254f248ddf499795df91e
SAF <stanleyfamilleza@gmail.com>
2026-09-09 15:16:14 +0700

    Show entry feedback toasts

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,10 +1,12 @@
 import { AppShell } from "@/components/app-shell";
 import { PrototypeStoreProvider } from "@/components/prototype-store";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <PrototypeStoreProvider>
       <AppShell>{children}</AppShell>
+      <Toaster />
     </PrototypeStoreProvider>
   );
 }
