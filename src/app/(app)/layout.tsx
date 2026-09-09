ed2bce1c82ccb3b2ed8796ca0241c93b603a50a8
SAF <stanleyfamilleza@gmail.com>
2026-09-09 15:22:48 +0700

    Wire shadcn Base UI toast

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 import { AppShell } from "@/components/app-shell";
 import { PrototypeStoreProvider } from "@/components/prototype-store";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toast";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
